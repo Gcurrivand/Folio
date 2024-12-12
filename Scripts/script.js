6f23fcafdd4f56b8fcaa1f685294589b255ffe65
Gcurrivand <g.currivand@gmail.com>
2024-12-12 01:44:47 +0100

    Huge design update + own routing framework

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,12 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const canvas = document.getElementById('three-canvas');
-  const scrollDownElement = document.getElementById('scroll-down');
-  const menuButton = document.querySelector(".menu-button");
-  const dropdownMenu = document.querySelector(".dropdown-menu");
-  const titles = document.querySelectorAll(".title");
-  const main = document.querySelector("main");
-
+document.addEventListener('DOMContentLoaded', async () => {
   const loadingScreen = document.getElementById("loading-screen");
+  const mainContent = document.querySelector(".main-content");
+    // Load default content from home.html
+    try {
+      const response = await fetch('../Views/home.html');
+      const data = await response.text();
+      mainContent.innerHTML = data;
+    } catch (error) {
+      console.error('Error loading default home.html:', error);
+    }
 
   window.addEventListener("load", () => {
     if (loadingScreen) {
@@ -18,74 +20,36 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
-  titles.forEach((title, index) => {
-    const menuItem = document.createElement("a");
-    menuItem.href = `#title-${index}`;
-    menuItem.textContent = title.textContent;
-    dropdownMenu.appendChild(menuItem);
-    title.id = `title-${index}`;
-  });
-
-  menuButton.addEventListener("click", () => {
-    dropdownMenu.classList.toggle("hidden");
-  });
-
-  dropdownMenu.addEventListener("click", (e) => {
-    if (e.target.tagName === "A") {
-      e.preventDefault();
-      const targetId = e.target.getAttribute("href").substring(1);
-      const targetElement = document.getElementById(targetId);
-      canvas.style.opacity = '0';
-      canvas.dataset.forcecamera = true;
-
-      targetElement.scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-      });
-
-      dropdownMenu.classList.add("hidden");
-    }
-  });
-
-  let lastTouchY = 0;
-
-  function handleScroll(yOffsetChange) {
-    const canvasOpacity = parseFloat(window.getComputedStyle(canvas).opacity);
-    if (window.scrollY > 8500) {
-      scrollDownElement.style.display = 'none';
-    } else {
-      scrollDownElement.style.display = 'block';
-    }
-
-    if (canvasOpacity > 0) {
-      return true; // Prevent default scrolling
-    } else if ((window.scrollY <= 0 || window.scrollY === undefined) && yOffsetChange < 0) {
-      canvas.style.opacity = '0.01';
-      canvas.style.zIndex = '1';
-      return true;
-    }
-
-    return false; // Allow default scrolling
-  }
-
-  main.addEventListener('touchstart', (event) => {
-    lastTouchY = event.touches[0].clientY;
-  });
-
-  main.addEventListener('touchmove', (event) => {
-    const touchY = event.touches[0].clientY;
-    const yOffsetChange = lastTouchY - touchY;
-    lastTouchY = touchY;
-    handleScroll(yOffsetChange)
-  });
-
-  window.addEventListener('wheel', (event) => {
-    if (handleScroll(event.deltaY)) {
-      event.preventDefault();
-    }
-  }, { passive: false });
-
   window.addEventListener('beforeunload', () => {
     window.scrollTo(0, 0);
   });
+
+  /*ROUTING*/
+  const homeLinks = document.querySelectorAll(".home-link");
+  homeLinks.forEach(homeLink => {
+    homeLink.addEventListener('click', async () => {
+      try {
+        const response = await fetch('../Views/home.html');
+        const data = await response.text();
+        mainContent.innerHTML = data;
+      } catch (error) {
+        console.error('Error loading home.html:', error);
+      }
+    });
+  });
+
+  const projectsLinks = document.querySelectorAll(".projects-link");
+  projectsLinks.forEach(projectsLink => {
+    projectsLink.addEventListener('click', async () => {
+      try {
+        const response = await fetch('../Views/projects.html');
+        const data = await response.text();
+        mainContent.innerHTML = data;
+      } catch (error) {
+        console.error('Error loading home.html:', error);
+      }
+    });
+  });
+
+
 });
